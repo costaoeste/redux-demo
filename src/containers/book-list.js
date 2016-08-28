69892f457e1dc085b5899c95aa2079dd1140bacf
Pablo <fortunatus26@gmail.com>
2016-08-28 13:30:20 +0200

    redux working

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component{
 
   renderList(){
     return this.props.books.map( (book) => {
       return (
-        <li key={book.titke} className="list-group-item">{book.title}</li>
+        <li
+        key={book.title}
+        onClick={ () => this.props.selectBook(book)}
+        className="list-group-item">{book.title}</li>
       );
     });
   }
@@ -25,5 +30,9 @@ class BookList extends Component{
       books:state.books
     };
   }
-  
-export default connect (mapStateToProps)(BookList);
+
+  function mapDispatchToProps(dispatch){
+    return bindActionCreators({selectBook:selectBook} , dispatch);
+  }
+
+export default connect (mapStateToProps , mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function (){
   return[
-    {title:"Javascript : The good parts"},
-    {title:"Harry potter"},
-    {title:"The Dark Tower"},
-    {title:"Clean code"}
+    {title:"Javascript : The good parts" , pages:101},
+    {title:"Harry potter",pages:310},
+    {title:"The Dark Tower",pages:240},
+    {title:"Clean code",pages:600}
   ]
 }
